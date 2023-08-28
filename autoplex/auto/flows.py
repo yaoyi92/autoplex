67617bf2c9a3659b15a0d0fd1be51e2018ae2e42
QuantumChemist <c.zitlau@live.com>
2023-08-28 18:17:41 +0200

    put debugger in ins own file

--- autoplex/auto/flows.py
+++ autoplex/auto/flows.py
@@ -90,18 +90,3 @@ class PhononDFTMLBenchmarkFlow(Maker):
         flow = Flow(flows)
         return flow
 
-@dataclass
-class debugger(Maker):
-
-    name: str = "debug"
-
-    def make(self, structure_list: list[Structure],):
-
-        jobs = []
-        fitinput = {'dft': ['sv3006:/home/certural/Calc/block_2023-08-23-11-35-04-773632/launcher_2023-08-25-09-39-24-719380', 'sv3012:/home/certural/Calc/block_2023-08-23-11-35-04-773632/launcher_2023-08-25-09-39-19-468652'], 'random': ['sv3020:/home/certural/Calc/block_2023-08-23-11-35-04-773632/launcher_2023-08-25-09-31-48-299628', 'sv3001:/home/certural/Calc/block_2023-08-23-11-35-04-773632/launcher_2023-08-25-09-31-43-108063']}
-
-        MLfit = MLIPFitMaker(name="GAP").make(species_list=["Li", "Cl"], iso_atom_energy=[-0.28665266, -0.25639058], fitinput=fitinput, structurelist=structure_list)
-        jobs.append(MLfit)
-
-        flow = Flow(jobs)
-        return flow
