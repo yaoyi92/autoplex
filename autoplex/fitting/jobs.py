3e94dec606fcd4ac3508506ec60b62d12180cd82
anaik <anaik@sv2218.zit.bam.de>
2023-10-10 15:43:38 +0200

    add doc strings and type hints

--- autoplex/fitting/jobs.py
+++ autoplex/fitting/jobs.py
@@ -16,24 +16,48 @@ from autoplex.fitting.utils import (
     gap_hyperparameter_constructor,
 )
 
-CurrentDir = Path(__file__).absolute().parent
+current_dir = Path(__file__).absolute().parent
 
 
 @job
 def gapfit(
-    fitinput: dict,
-    isolatedatoms,
-    isolatedatomsenergy,
-    gap_input=CurrentDir / "gap-defaults.json",
-    twobody: bool = True,
-    threebody: bool = False,
+    fit_input: dict,
+    isolated_atoms: list,
+    isolated_atoms_energy: list,
+    path_to_default_hyperparameters: Path | str = current_dir / "gap-defaults.json",
+    two_body: bool = True,
+    three_body: bool = False,
     soap: bool = True,
     fit_kwargs: dict = field(default_factory=dict),
 ):
     """
-    job that prepares GAP fit input and fits the data using GAP. More ML methods (e.g. ACE) to follow.
+    Prepares the GAP fit input and fits the data using GAP. More ML methods (e.g. ACE) to follow.
 
+    Parameters
+    ----------
+    fit_input : dict.
+        list containing static calculation directories.
+    isolated_atoms : list.
+        List of isolated atoms
+    isolated_atoms_energy : list.
+        List of isolated atoms energy
+    path_to_default_hyperparameters : str or Path.
+        Path to gap-defaults.json.
+    two_body : bool.
+        bool indicating whether to include two-body hyperparameters
+    three_body : bool.
+        bool indicating whether to include three-body hyperparameters
+    soap : bool.
+        bool indicating whether to include soap hyperparameters
+    fit_kwargs : dict.
+        dict including gap fit keyword args.
+
+    Returns
+    -------
+    Response.output
+        Path to the gap fit file.
     """
+
     flattened_input = lambda x: [
         y
         for z in x
@@ -42,7 +66,7 @@ def gapfit(
     fit = flattened_input(
         [
             dirs
-            for data in fitinput.values()
+            for data in fit_input.values()
             for datatype, dirs in data.items()
             if datatype != "phonon_data"
         ]
@@ -60,20 +84,17 @@ def gapfit(
             i.pbc = True
         write("trainGAP.xyz", file, append=True)
 
-    # with open(gap_input, "r") as infile:
-    #     inputs = json.load(infile)
-
     gap_default_hyperparameters = load_gap_hyperparameter_defaults(
-        gap_fit_parameter_file_path=gap_input
+        gap_fit_parameter_file_path=path_to_default_hyperparameters
     )
 
     e0: str = "{"
 
-    for isoatom, isoenergy in zip(isolatedatoms, isolatedatomsenergy):
-        if isoatom == isolatedatoms[-1]:
-            e0 += str(isoatom) + ":" + str(isoenergy) + "}"
+    for iso_atom, iso_energy in zip(isolated_atoms, isolated_atoms_energy):
+        if iso_atom == isolated_atoms[-1]:
+            e0 += str(iso_atom) + ":" + str(iso_energy) + "}"
         else:
-            e0 += str(isoatom) + ":" + str(isoenergy) + ":"
+            e0 += str(iso_atom) + ":" + str(iso_energy) + ":"
     # Updating the isolated atom energy
     gap_default_hyperparameters["general"].update({"e0": e0})
     # Overwriting the default gap_fit settings with user settings  # TODO XPOT support
@@ -84,13 +105,11 @@ def gapfit(
 
     gap = gap_hyperparameter_constructor(
         gap_parameter_dict=gap_default_hyperparameters,
-        two_body=twobody,
-        three_body=threebody,
+        two_body=two_body,
+        three_body=three_body,
         soap=soap,
     )
-    # gap: str = GAPHyperparameterParser(
-    #     inputs=inputs, twobody=twobody, threebody=threebody, soap=soap
-    # )
+
     general = [
         str(key) + "=" + str(gap_default_hyperparameters["general"][key])
         for key in gap_default_hyperparameters["general"]
@@ -106,25 +125,3 @@ def gapfit(
             os.path.join(directory, gap_default_hyperparameters["general"]["gp_file"])
         )
     )
-
-
-#
-# def GAPHyperparameterParser(
-#     inputs, twobody: bool = True, threebody: bool = False, soap: bool = True
-# ):
-#     twob: str = " ".join(
-#         [f"{key}={value}" for key, value in inputs["twob"].items() if twobody is True]
-#     )
-#     threeb: str = " ".join(
-#         [
-#             f"{key}={value}"
-#             for key, value in inputs["threeb"].items()
-#             if threebody is True
-#         ]
-#     )
-#     SOAP: str = str(":soap " if soap is True else "") + " ".join(
-#         [f"{key}={value}" for key, value in inputs["soap"].items() if soap is True]
-#     )
-#     gap: str = "gap={" + (twob + threeb + SOAP) + "}"
-#
-#     return gap
