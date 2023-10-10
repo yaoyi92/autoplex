d2f817fa2da099750e79d9dc54e3cfe27d5cddb1
anaik <anaik@sv2218.zit.bam.de>
2023-10-10 16:51:51 +0200

    remove unused unittest

--- tests/data/test_datagen_flows.py
+++ tests/data/test_datagen_flows.py
@@ -1,38 +1,36 @@
 from __future__ import annotations
 
-import unittest
 import inspect
 from pymatgen.core.structure import Structure
 from autoplex.data.flows import DataGenerator, IsoAtomMaker
 
 
-class TestDataGen(unittest.TestCase):
-    def test_datageneration(self):
-        test_structure = Structure(
-            lattice=[
-                [3.0, 0.0, 0.0],
-                [0.0, 3.0, 0.0],
-                [0.0, 0.0, 3.0],
-            ],
-            species=["Mo", "C", "K"],
-            coords=[[0.66, 0.66, 0.66], [0.33, 0.33, 0.33], [0, 0, 0]],
-        )
-        test_mpid = "mp-test"
-        test_species = test_structure.types_of_species
+def test_datageneration():
+    test_structure = Structure(
+        lattice=[
+            [3.0, 0.0, 0.0],
+            [0.0, 3.0, 0.0],
+            [0.0, 0.0, 3.0],
+        ],
+        species=["Mo", "C", "K"],
+        coords=[[0.66, 0.66, 0.66], [0.33, 0.33, 0.33], [0, 0, 0]],
+    )
+    test_mpid = "mp-test"
+    test_species = test_structure.types_of_species
 
-        data = DataGenerator().make(structure=test_structure, mpid=test_mpid)
-        isoatom = []
-        for species in test_species:
-            isoatom.append(IsoAtomMaker().make(species))
+    data = DataGenerator().make(structure=test_structure, mpid=test_mpid)
+    isoatom = []
+    for species in test_species:
+        isoatom.append(IsoAtomMaker().make(species))
 
-        assert (
-            len(data.jobs) == 2
-        )  # not sure how else checking if job is submitted correctly without actually running VASP jobs
-        assert len((isoatom)) == 3
-        assert [
-            items.default
-            for items in inspect.signature(DataGenerator).parameters.values()
-            if items.name == "sc"
-        ][
-            0
-        ] is False  # important to avoid accidentally wayyy too large workflows
+    assert (
+        len(data.jobs) == 2
+    )  # not sure how else checking if job is submitted correctly without actually running VASP jobs
+    assert len((isoatom)) == 3
+    assert [
+        items.default
+        for items in inspect.signature(DataGenerator).parameters.values()
+        if items.name == "sc"
+    ][
+        0
+    ] is False  # important to avoid accidentally way too large workflows
