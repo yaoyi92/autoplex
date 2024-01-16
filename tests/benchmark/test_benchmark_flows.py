ed97603b2de03e3be1065f418d3c1fecfab52e67
QuantumChemist <c.zitlau@live.com>
2024-01-16 18:09:23 +0100

    fixed tests in test_benchmark_flows.py

--- tests/benchmark/test_benchmark_flows.py
+++ tests/benchmark/test_benchmark_flows.py
@@ -28,7 +28,7 @@ def test_benchmark(test_dir, clean_dir):
     responses = run_locally(benchmark_flow, create_folders=False, ensure_success=True)
 
     assert responses[benchmark_flow.output.uuid][1].output == pytest.approx(
-        22.572177275621236
+        0.5716963823412201
     )
 
     # get list of generated plot files
