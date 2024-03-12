a70527fe8fa224b800d707a357f9966d73c91df3
QuantumChemist <c.zitlau@live.com>
2024-03-12 16:05:23 +0100

    added possibility to resize the cell to generate_randomized_structures

--- autoplex/data/phonons/jobs.py
+++ autoplex/data/phonons/jobs.py
@@ -16,6 +16,7 @@ from pymatgen.io.ase import AseAtomsAdaptor
 def generate_randomized_structures(
     structure: Structure,
     n_struct: int,
+    cell_factor: float = 1.0,
 ):
     """
     Take in a structure object and generates randomly displaced structure.
@@ -26,6 +27,8 @@ def generate_randomized_structures(
         Pymatgen structures object.
     n_struct : int.
         Total number of randomly displaced structures to be generated.
+    cell_factor: float
+        factor to resize cell parameters.
 
     Returns
     -------
@@ -34,6 +37,7 @@ def generate_randomized_structures(
     """
     random_rattled = []
     ase_structure = AseAtomsAdaptor.get_atoms(structure)
+    ase_structure.set_cell(ase_structure.get_cell() * cell_factor, scale_atoms=True)
     for seed in np.random.permutation(100000)[:n_struct]:
         ase_structure.rattle(seed=seed, stdev=0.01)
         random_rattled.append(AseAtomsAdaptor.get_structure(ase_structure))
