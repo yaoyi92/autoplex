979f09c956049351d98b969f2293009eca52c573
JaGeo <janinegeorge.ulfen@gmail.com>
2024-10-05 12:09:07 +0200

    tryingto set all openblas libraries to unparallel use

--- autoplex/fitting/common/utils.py
+++ autoplex/fitting/common/utils.py
@@ -1688,7 +1688,10 @@ def run_gap(num_processes_fit: int, parameters) -> None:
 
     """
     os.environ["OMP_NUM_THREADS"] = str(num_processes_fit)
-    os.environ["OPENBLAS_NUM_THREADS"] = "1"  # export OPENBLAS_NUM_THREADS=1
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"  # blas library
+    os.environ["BLIS_NUM_THREADS"] = "1"  # blas library
+    os.environ["MKL_NUM_THREADS"] = "1"  # blas library
+    os.environ["NETLIB_NUM_THREADS"] = "1"  # blas library
 
     with open("std_gap_out.log", "w", encoding="utf-8") as file_std, open(
         "std_gap_err.log", "w", encoding="utf-8"
@@ -1715,7 +1718,10 @@ def run_quip(
 
     """
     os.environ["OMP_NUM_THREADS"] = str(num_processes_fit)
-    os.environ["OPENBLAS_NUM_THREADS"] = "1"  # export OPENBLAS_NUM_THREADS=1
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"  # blas library
+    os.environ["BLIS_NUM_THREADS"] = "1"  # blas library
+    os.environ["MKL_NUM_THREADS"] = "1"  # blas library
+    os.environ["NETLIB_NUM_THREADS"] = "1"  # blas library
 
     init_args = "init_args='IP Glue'" if glue_xml else ""
     quip = (
