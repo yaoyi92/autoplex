60b7a4611338dd6e08d54b8e14c3fed015d5620f
naik-aakash <aakash.naik@bam.de>
2024-11-27 14:40:48 +0100

    raise error on lammps import error

--- src/autoplex/data/rss/utils.py
+++ src/autoplex/data/rss/utils.py
@@ -407,6 +407,16 @@ def process_rss(
     elif mlip_type == "J-ACE":
         from ase.calculators.lammpslib import LAMMPSlib
 
+        try:
+            pass
+        except Exception as exc:
+            raise ImportError(
+                "To use RSS flow with J-ACE potential, it needs LAMMPS compiled with"
+                "python bindings and lammps-user-pace. Please follow the instructions in"
+                "the autoplex Documentation to install LAMMPS with the required packages."
+                "https://autoatml.github.io/autoplex/user/index.html#lammps-installation"
+            ) from exc
+
         ace_label = os.path.join(mlip_path, "acemodel.yace")
         ace_json = os.path.join(mlip_path, "acemodel.json")
         ace_table = os.path.join(mlip_path, "acemodel_pairpot.table")
