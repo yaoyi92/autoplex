7330f006bb5070d83945b5800cd8ecf13c1d00df
QuantumChemist <c.zitlau@live.com>
2024-12-05 17:10:18 +0100

    remove future annotations import

--- src/autoplex/benchmark/phonons/flows.py
+++ src/autoplex/benchmark/phonons/flows.py
@@ -1,17 +1,10 @@
 """Flows to benchmark ML potentials."""
 
-from __future__ import annotations
-
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
 
-from jobflow import Maker
-
-if TYPE_CHECKING:
-    from atomate2.common.schemas.phonons import PhononBSDOSDoc
-    from pymatgen.core.structure import Structure
-
-from jobflow import job
+from atomate2.common.schemas.phonons import PhononBSDOSDoc
+from jobflow import Maker, job
+from pymatgen.core.structure import Structure
 
 from autoplex.benchmark.phonons.utils import compute_bandstructure_benchmark_metrics
 
