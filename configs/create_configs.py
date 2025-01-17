0eabca30bc47ef3d4898da8ccfc7aa5434b2e22a
naik-aakash <aakash.naik@bam.de>
2025-01-17 23:17:04 +0100

    add example script to root

---
+++ configs/create_configs.py
@@ -0,0 +1,9 @@
+"""Example script to create hyperparameters and rss config objects using json/yaml files."""
+
+from autoplex.settings import MLIPHypers, RssConfig
+
+# create a custom hyperparameters object using json file
+custom_hyperparameters = MLIPHypers.from_file("mlip_hypers.json")
+
+# create a custom rss config object using json file
+custom_rss_config = RssConfig.from_file("rss_config.yaml")
