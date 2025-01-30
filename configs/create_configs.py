09cffbf8f1670c9651b171f33015a7aeefab6a60
naik-aakash <aakash.naik@bam.de>
2025-01-30 11:22:12 +0100

    update example config files

--- configs/create_configs.py
+++ configs/create_configs.py
@@ -6,4 +6,6 @@ from autoplex.settings import MLIPHypers, RssConfig
 custom_hyperparameters = MLIPHypers.from_file("mlip_hypers.json")
 
 # create a custom rss config object using json file
-custom_rss_config = RssConfig.from_file("rss_config.yaml")
+custom_rss_config = RssConfig.from_file(
+    "rss_config.yaml"
+)  # rss_config_all.yaml contains list of all hypers and supported MLIPs
