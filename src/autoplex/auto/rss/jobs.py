a9dcd30df4ce32723b067d237f98decba892f092
naik-aakash <aakash.naik@bam.de>
2025-01-30 18:46:26 +0100

    remove redundant variable creation

--- src/autoplex/auto/rss/jobs.py
+++ src/autoplex/auto/rss/jobs.py
@@ -171,6 +171,8 @@ def initial_rss(
     if dimer_box is None:
         dimer_box = [20.0, 20.0, 20.0]
 
+    print(buildcell_options)
+
     do_randomized_structure_generation = BuildMultiRandomizedStructure(
         generated_struct_numbers=generated_struct_numbers,
         buildcell_options=buildcell_options,
@@ -601,13 +603,11 @@ def do_rss_iterations(
         if include_dimer:
             include_dimer = False
 
-        (mlip_path,) = do_mlip_fit.output["mlip_path"]
-
         do_iteration = do_rss_iterations(
             input={
                 "test_error": do_mlip_fit.output["test_error"],
                 "pre_database_dir": do_data_preprocessing.output,
-                "mlip_path": mlip_path,
+                "mlip_path": do_mlip_fit.output["mlip_path"],
                 "isolated_atom_energies": input["isolated_atom_energies"],
                 "current_iter": current_iter,
                 "kt": kt,
